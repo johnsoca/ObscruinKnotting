48bb9822437d5143a508457cece2d890fa35af11
Callie J Miller, PhD <cgator@gmail.com>
2021-07-16 15:13:01 -0400

    Modified to account for normal distributions of angles between domain and linker and bimodal distribution of possible angles between linker and domain.

--- ObscurinKnotting.m
+++ ObscurinKnotting.m
@@ -4,22 +4,31 @@
 % Includes the function(s): plus_minus.m
 
 % Variables:
-% phi- angle between the head and the tail of a single node
-% x - x position of the tip of the head
-% y - y position of the tip of the head
-% theta_h - angle of the head
-% x_e, y_e - x and y coordinates of the end of the head, also the start of
-%            the tail linker- head is 30 Angstroms in length
-% xt, yt - x and y coordinates of the end of the tail- tail is 18 angstroms
+% phi- angle between the domain and the linker of a single node: randomly generated according to normal
+%       distribution mu=126, sigma=18.5 degrees
+% x - x position of the tip of the domain
+% y - y position of the tip of the domain
+% theta_h - angle of the domain
+% x_e, y_e - x and y coordinates of the end of the domain, also the start of
+%            the linker- domain is 30 Angstroms in length
+% xt, yt - x and y coordinates of the end of the linker- linker is 18 angstroms
 %          in length
-% alpha - randomly generated to be the angle between 75 and 180 between the
-%         tail of one linker and the head of the next linker
-% N - number of nodes which include a head and a tail
+% alpha - randomly generated according to a bi-modal normal distribution
+%           with mu=93.3 and sigma=13.2, and mu=58.4 and sigma=9.44 (degrees) as the
+%           angle between the linker to the next domain
+% N - number of nodes which include a domain and a linker
 
-sims=1;
+sims=1000000;
 x_range = zeros(1,sims);
 y_range = zeros(1,sims);
 
+% Angle distribution values
+mu_d2l = deg2rad(126); %mu of domain to linker
+s_d2l = deg2rad(18.5); %sigma of domain to linker
+mu_l2d = deg2rad([93.3,58.4]); %mu of linker to domain bimodal distribution
+s_l2d = deg2rad([13.2,9.44]); %sigma of linker to domain bimodal distribution
+
+tic
 for sim = 1:sims
     % Initialize variables
     N = 18;
@@ -32,28 +41,29 @@ for sim = 1:sims
     xt = zeros(1,18);
     yt = zeros(1,18);
 
-    % Initialize the first position and angle of the first head of node 1
+    % Initialize the first position and angle of the first domain of node 1
     x(1) = rand();
     y(1) = rand();
-    theta_h(1) = plus_minus * rand()*pi; 
+    theta_h(1) = plus_minus * rand()*pi; % initial orienation of domain doens't matter 
 
-    figure()
-    hold on
+%    figure()
+%    hold on
 
     for i = 1: N
         x_e(i) = x(i) + 30*cos(theta_h(i));
         y_e(i) = y(i) + 30*sin(theta_h(i));
 
-        phi(i) = plus_minus * rand() * acos(8/18); 
+        phi(i) = plus_minus * normrnd(mu_d2l,s_d2l) * acos(8/18); 
         xt(i) = x_e(i) + 18*cos(theta_h(i)+phi(i));
         yt(i) = y_e(i) + 18*sin(theta_h(i)+phi(i));
 
-        plot([x(i),x_e(i)],[y(i),y_e(i)],'k');
-        plot([x_e(i),xt(i)],[y_e(i),yt(i)],'r+:');
+%         plot([x(i),x_e(i)],[y(i),y_e(i)],'k');
+%         plot([x_e(i),xt(i)],[y_e(i),yt(i)],'r+:');
         if i ~=N
             x(i+1) = xt(i);
             y(i+1) = yt(i);
-            alpha = deg2rad(plus_minus * (105*rand()+75)); % randomly generate an angle (in radians) between 75 and 180 degrees for the angle between the tail of a node and the head of the next node
+            bimodal = round(rand()+1); % 50% probability of choosing bimodal distribution index 1 as 2
+            alpha = plus_minus*normrnd(mu_l2d(bimodal),s_l2d(bimodal)); 
             beta = acos((x(i+1)-x_e(i))/18); % angle of the tail linker wrt the x axis
             theta_h(i+1) = pi-(beta+alpha);
         end
@@ -61,3 +71,8 @@ for sim = 1:sims
     x_range(sim) = max(x)-min(x);
     y_range(sim) = max(y)-min(y);
 end
+figure()
+hist(x_range);
+figure()
+hist(y_range);
+toc
