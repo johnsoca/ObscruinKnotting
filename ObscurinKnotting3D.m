543c6e53577d4493f5b3443d872b5f53c64d56de
johnsoca <cgator@gmail.com>
2021-10-01 11:12:25 -0400

    continuing to build out the 3D solver code with the need to solve the vector system of equations

--- ObscurinKnotting3D.m
+++ ObscurinKnotting3D.m
@@ -26,11 +26,12 @@
 % N - number of nodes which include a domain and a linker
 % l_l - length of linker, 12.8 Angstroms
 % l_d - length of domain, between 32-34 Angstroms, *** choose to be 33 ***
+% tau - random with each call, determines the degree of "twist" as defined
+%   from the angle between the vector and the z-axis.
 
+%--MONTE CARLO--
 %sims=1000000;
-%x_range = zeros(1,sims);
-%y_range = zeros(1,sims);
-%crossings = zeros(1,sims);
+%---------------
 
 % Angle distribution values
 mu_d2l = deg2rad(126); %mu of node domain to linker
@@ -41,115 +42,36 @@ s_l2d = deg2rad([13.2,9.44]); %sigma of linker to domain bimodal distribution
 % Initialize other variables
 l_l = 12.8;
 l_d = 33;
+N = 18; %number of IG domain-linker pairs, i.e. one node
 
 
-%tic
-%m=1;
+%-- MONTE CARLO--
 %for sim = 1:sims
     % Initialize variables
-    N = 18;
     phi = zeros(1,N);
     x = zeros(1,N);
     y = zeros(1,N);
     z = zeros(1,N);
-    theta_h = zeros(1,N);
     x_e = zeros(1,N);
     y_e = zeros(1,N);
     z_e = zeros(1,N);
     xt = zeros(1,N);
     yt = zeros(1,N);
     zt = zeros(1,N);
-%     X = zeros(1,37);
-%     Y = zeros(1,37);
+
 
     % Initialize the first position and angle of the first domain of node 1
     x(1) = rand();
     y(1) = rand();
-    theta_h(1) = plus_minus * rand()*pi; % initial orienation of domain doens't matter 
-
-%    %********** COMMENT IF RUNNING MONTE CARLO
-%    figure()
-%    hold on
-%    %*********
-    for i = 1: N
-        x_e(i) = x(i) + 30*cos(theta_h(i));
-        y_e(i) = y(i) + 30*sin(theta_h(i));
-
-        phi(i) = plus_minus * normrnd(mu_d2l,s_d2l) * acos(8/18); 
-        xt(i) = x_e(i) + 18*cos(theta_h(i)+phi(i));
-        yt(i) = y_e(i) + 18*sin(theta_h(i)+phi(i));
-        
-%         % ********************
-%         plot([x(i),x_e(i)],[y(i),y_e(i)],'k');
-%         plot([x_e(i),xt(i)],[y_e(i),yt(i)],'r+:');
-%         % ********************
-        
-        if i ~=N
-            x(i+1) = xt(i);
-            y(i+1) = yt(i);
-            bimodal = round(rand()+1); % 50% probability of choosing bimodal distribution index 1 as 2
-            alpha = plus_minus*normrnd(mu_l2d(bimodal),s_l2d(bimodal)); 
-            beta = acos((x(i+1)-x_e(i))/18); % angle of the tail linker wrt the x axis
-            theta_h(i+1) = pi-(beta+alpha);
-        end
-    end
-    x_range(sim) = max(x)-min(x);
-    y_range(sim) = max(y)-min(y);
+    z(1) = rand();
+    alpha = plus_minus * rand()*pi; % for 3D space, need to randomly declare alpha as angle between the first domain and x-axis
+    beta = plus_minus * rand()*pi; % for 3D space, need to randomly declare beta as angle between the first domain and y-axis
+    gamma = plus_minus * rand()*pi; % for 3D space, need to randomly declare gamma as angle between the first domain and z-axis
     
-
-    % -------------------
-    % Code below is used to test if a "cross-over" has occurred,
-    % and count the instances of cross-overs
-    %--------------------
-    s = 1;
-    for i = 1 : N
-        X(s) = x(i);
-        X(s+1) = x_e(i);
-        Y(s) = y(i);
-        Y(s+1) = y_e(i);
-        s=s+2;
-    end
-    X(37) = xt(18);
-    Y(37) = yt(18);
+    x_e(1)=x(1)+l_d*cos(alpha);
+    y_e(1)=y(1)+l_d*cos(beta);
+    z_e(1)=z(1)+l_d*cos(gamma);
     
-    count=0;
-    for s = 1:length(X)-3
-        x1 = X(s);
-        y1 = Y(s);
-        x2 = X(s+1);
-        y2 = Y(s+1);
-        for n = s+2: length(X)-1
-            x3 = X(n);
-            y3 = Y(n);
-            x4 = X(n+1);
-            y4 = Y(n+1);
-            c = crossovers(x1,y1,x2,y2,x3,y3,x4,y4);
-            count = count + c;
-        end
-    end
-    
-    crossings(sim)=count;
-%     if crossings(sim)>100
-%         dX(m,:)=X;
-%         dY(m,:)=Y;
-%         dtheta(m,:)=theta_h;
-%         dphi(m,:)=phi;
-%         CROSSINGCHECK(m) = crossings(sim);
-%         m=m+1;
-%     end
-end
-
-%------------------------------------
-% Plot the histograms of the "spread" in the nodes in the x and y
-% directions to determine the likelihood that an 18-domain/linker node is
-% stretched or crumpled on itself
-%------------------------------------
-%figure()
-%hist(x_range);
-%figure()
-%hist(y_range);
-figure()
-hist(crossings);
-figure()
-hist(crossings/N);
-toc
+    tau=plus_mins*rand()*pi; % random each time called
+    zt(1)=l_d*cos(tau)+z_e(1);
+    [yt(1),xt(1)]=multipleEqnSolver(
