7269a2a4fe7b76770288ead69fc8132dd1f84288
johnsoca <cgator@gmail.com>
2021-10-01 11:02:04 -0400

    updated header language for clarity

--- ObscurinKnotting3D.m
+++ ObscurinKnotting3D.m
@@ -1,49 +1,66 @@
-% ObscurinKnotting.m
+% ObscurinKnotting3D.m
 % Callie J Miller
-% 06/18/21
+% 10/01/21
+% The purpose of this code is to simulate the node-linker morphologies
+% based on data from YASARA for obscurin.
+% Definitions:
+%   domain- Ig domain
+%   linker- Linker between Ig domains
+%
 % Includes the function(s): plus_minus.m
 
 % Variables:
-% phi- angle between the domain and the linker of a single node: randomly generated according to normal
+% phi- angle between the domain and the following linker: randomly generated according to normal
 %       distribution mu=126, sigma=18.5 degrees
 % x - x position of the tip of the domain
-% y - y position of the tip of the domain
-% theta_h - angle of the domain
-% x_e, y_e - x and y coordinates of the end of the domain, also the start of
-%            the linker- domain is 30 Angstroms in length
-% xt, yt - x and y coordinates of the end of the linker- linker is 18 angstroms
+% y - y position of the tip of the domain node
+% z - z position of the tip of the domain node
+% theta - 
+% x_e, y_e, z_e - x, y and z coordinates of the end of the domain node, also the start of
+%            the linker- domain is between 32-34 Angstroms in length
+% xt, yt, zt - x, y and z coordinates of the end of the linker- linker is 12.8 angstroms
 %          in length
 % alpha - randomly generated according to a bi-modal normal distribution
 %           with mu=93.3 and sigma=13.2, and mu=58.4 and sigma=9.44 (degrees) as the
 %           angle between the linker to the next domain
 % N - number of nodes which include a domain and a linker
+% l_l - length of linker, 12.8 Angstroms
+% l_d - length of domain, between 32-34 Angstroms, *** choose to be 33 ***
 
-sims=1000000;
-x_range = zeros(1,sims);
-y_range = zeros(1,sims);
-crossings = zeros(1,sims);
+%sims=1000000;
+%x_range = zeros(1,sims);
+%y_range = zeros(1,sims);
+%crossings = zeros(1,sims);
 
 % Angle distribution values
-mu_d2l = deg2rad(126); %mu of domain to linker
-s_d2l = deg2rad(18.5); %sigma of domain to linker
+mu_d2l = deg2rad(126); %mu of node domain to linker
+s_d2l = deg2rad(18.5); %sigma of node domain to linker
 mu_l2d = deg2rad([93.3,58.4]); %mu of linker to domain bimodal distribution
 s_l2d = deg2rad([13.2,9.44]); %sigma of linker to domain bimodal distribution
 
-tic
-m=1;
-for sim = 1:sims
+% Initialize other variables
+l_l = 12.8;
+l_d = 33;
+
+
+%tic
+%m=1;
+%for sim = 1:sims
     % Initialize variables
     N = 18;
-    phi = zeros(1,18);
-    x = zeros(1,18);
-    y = zeros(1,18);
-    theta_h = zeros(1,18);
-    x_e = zeros(1,18);
-    y_e = zeros(1,18);
-    xt = zeros(1,18);
-    yt = zeros(1,18);
-    X = zeros(1,37);
-    Y = zeros(1,37);
+    phi = zeros(1,N);
+    x = zeros(1,N);
+    y = zeros(1,N);
+    z = zeros(1,N);
+    theta_h = zeros(1,N);
+    x_e = zeros(1,N);
+    y_e = zeros(1,N);
+    z_e = zeros(1,N);
+    xt = zeros(1,N);
+    yt = zeros(1,N);
+    zt = zeros(1,N);
+%     X = zeros(1,37);
+%     Y = zeros(1,37);
 
     % Initialize the first position and angle of the first domain of node 1
     x(1) = rand();
