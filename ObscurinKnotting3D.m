d8f5d71e1cd51042ae86bc97119856cd59589c58
johnsoca <cgator@gmail.com>
2021-10-19 11:38:49 -0400

    Monte Carlo 100 simulations to determine spread of 18 node Obscurin

--- ObscurinKnotting3D.m
+++ ObscurinKnotting3D.m
@@ -29,7 +29,7 @@
 %   from the angle between the vector and the z-axis.
 
 %--MONTE CARLO--
-%sims=1000000;
+sims=100000;
 %---------------
 
 % Angle distribution values
@@ -41,11 +41,16 @@ s_l2d = deg2rad([13.2,9.44]); %sigma of linker to domain bimodal distribution
 % Initialize other variables
 l_l = 12.8;
 l_d = 33;
-N = 2; % 18; %number of IG domain-linker pairs, i.e. one node
+N = 18; %number of IG domain-linker pairs, i.e. one node
 
+tic
+%for plotting the range of x, y, and z values
+x_range=zeros(1,sims);
+y_range=zeros(1,sims);
+z_range=zeros(1,sims);
 
 %-- MONTE CARLO--
-%for sim = 1:sims
+for sim = 1:sims
     % Initialize variables
     phi = zeros(1,N);
     x = zeros(1,N);
@@ -59,7 +64,6 @@ N = 2; % 18; %number of IG domain-linker pairs, i.e. one node
     zt = zeros(1,N);
     theta = zeros(1,N);
     phi = zeros(1,N);
-    tau=zeros(1,2*N);
 
 
     % Initialize the first position and angle of the first domain of node 1
@@ -70,68 +74,87 @@ N = 2; % 18; %number of IG domain-linker pairs, i.e. one node
     theta(1) = plus_minus*normrnd(mu_l2d(bimodal),s_l2d(bimodal));
     %use spherical coordinate system where tau is the inclination and theta
     %is the azimuth, and the magnitude is the radius
-    tau(1)=plus_minus*rand()*pi; % random each time called
-    x_e(1)=x(1)+l_d*cos(theta(1))*sin(tau(1));
-    y_e(1)=y(1)+l_d*sin(theta(1))*sin(tau(1));
-    z_e(1)=z(1)+l_d*cos(tau(1));
+    tau=plus_minus*rand()*pi; % random each time called
+    x_e(1)=x(1)+l_d*cos(theta(1))*sin(tau);
+    y_e(1)=y(1)+l_d*sin(theta(1))*sin(tau);
+    z_e(1)=z(1)+l_d*cos(tau);
      
-%###############CHECK#####################
-if abs((sqrt((x_e(1)-x(1))^2+(y_e(1)-y(1))^2+(z_e(1)-z(1))^2))-l_d)>0.01 %defined tolerance of "equal"
-    disp("first node is not within tolerance of the length of the Ig doman (l_d)");
-end
-%#########################################
+    %###############CHECK#####################
+    if abs((sqrt((x_e(1)-x(1))^2+(y_e(1)-y(1))^2+(z_e(1)-z(1))^2))-l_d)>0.01 %defined tolerance of "equal"
+        disp("first node is not within tolerance of the length of the Ig doman (l_d)");
+    end
+    %#########################################
 
     phi(1) = plus_minus * normrnd(mu_d2l,s_d2l);
-    tau(2)=plus_minus*rand()*pi; % random each time called
-    xt(1)=x_e(1)+l_l*cos(phi(1))*sin(tau(2));
-    yt(1)=y_e(1)+l_l*sin(phi(1))*sin(tau(2));
-    zt(1)=z_e(1)+l_l*cos(tau(2));
-    
-%###############CHECK#####################
-if abs((sqrt((xt(1)-x_e(1))^2+(yt(1)-y_e(1))^2+(zt(1)-z_e(1))^2))-l_l)>0.01 %defined tolerance of "equal"
-    disp("first linker is not within tolerance of the length of the linker doman (l_d)");
-end
-%#########################################  
+    tau=plus_minus*rand()*pi; % random each time called
+    xt(1)=x_e(1)+l_l*cos(phi(1))*sin(tau);
+    yt(1)=y_e(1)+l_l*sin(phi(1))*sin(tau);
+    zt(1)=z_e(1)+l_l*cos(tau);
+
+    %###############CHECK#####################
+    if abs((sqrt((xt(1)-x_e(1))^2+(yt(1)-y_e(1))^2+(zt(1)-z_e(1))^2))-l_l)>0.01 %defined tolerance of "equal"
+        disp("first linker is not within tolerance of the length of the linker doman (l_d)");
+    end
+    %#########################################  
 
     %update the beginning of the next domain such that it's the same as the
     %termination of the linker (xt,yt,zt)
-% maybe the beginning of the loop?
-    i=1; %******
-    if i ~=N
-        x(i+1) = xt(i);
-        y(i+1) = yt(i);
-        z(i+1) = zt(i);
+    for i=2:N
+        x(i) = xt(i-1);
+        y(i) = yt(i-1);
+        z(i) = zt(i-1);
         bimodal = round(rand()+1); % 50% probability of choosing bimodal distribution index 1 as 2
-        theta(i+1) = plus_minus*normrnd(mu_l2d(bimodal),s_l2d(bimodal)); 
+        theta(i) = plus_minus*normrnd(mu_l2d(bimodal),s_l2d(bimodal)); 
+        tau=plus_minus*rand()*pi;
+        x_e(i)=x(i)+l_d*cos(theta(i))*sin(tau);
+        y_e(i)=y(i)+l_d*sin(theta(i))*sin(tau);
+        z_e(i)=z(i)+l_d*cos(tau);
+
+    %###############CHECK#####################
+    if abs((sqrt((x_e(i)-x(i))^2+(y_e(i)-y(i))^2+(z_e(i)-z(i))^2))-l_d)>0.01 %defined tolerance of "equal"
+        disp("second node is not within tolerance of the length of the Ig doman (l_d)",i);
     end
-    tau(3)=plus_minus*rand()*pi;
-    x_e(2)=x(2)+l_d*cos(theta(2))*sin(tau(3));
-    y_e(2)=y(2)+l_d*sin(theta(2))*sin(tau(3));
-    z_e(2)=z(2)+l_d*cos(tau(3));
-   
-%###############CHECK#####################
-if abs((sqrt((x_e(2)-x(2))^2+(y_e(2)-y(2))^2+(z_e(2)-z(2))^2))-l_d)>0.01 %defined tolerance of "equal"
-    disp("second node is not within tolerance of the length of the Ig doman (l_d)");
-end
-%#########################################    
-    phi(2) = plus_minus * normrnd(mu_d2l,s_d2l);
-    tau(4)=plus_minus*rand()*pi; % random each time called
-    xt(2)=x_e(2)+l_l*cos(phi(2))*sin(tau(4));
-    yt(2)=y_e(2)+l_l*sin(phi(2))*sin(tau(4));
-    zt(2)=z_e(2)+l_l*cos(tau(4));
-%###############CHECK#####################
-if abs((sqrt((xt(2)-x_e(2))^2+(yt(2)-y_e(2))^2+(zt(2)-z_e(2))^2))-l_l)>0.01 %defined tolerance of "equal"
-    disp("second linker is not within tolerance of the length of the linker doman (l_d)");
-end
-%######################################### 
-% Troubleshooting 
-for i=1:N    
-    plot3([x(i),x_e(i)],[y(i),y_e(i)],[z(i),z_e(i)],'k');
-    hold on
-    plot3([x_e(i),xt(i)],[y_e(i),yt(i)],[z_e(i),zt(i)],'r+:');
-end
+    %#########################################    
+        phi(i) = plus_minus * normrnd(mu_d2l,s_d2l);
+        tau=plus_minus*rand()*pi; % random each time called
+        xt(i)=x_e(i)+l_l*cos(phi(i))*sin(tau);
+        yt(i)=y_e(i)+l_l*sin(phi(i))*sin(tau);
+        zt(i)=z_e(i)+l_l*cos(tau);
+    %###############CHECK#####################
+    if abs((sqrt((xt(i)-x_e(i))^2+(yt(i)-y_e(i))^2+(zt(i)-z_e(i))^2))-l_l)>0.01 %defined tolerance of "equal"
+        disp("second linker is not within tolerance of the length of the linker doman (l_d)",i);
+    end
+    %######################################### 
+
+    end
     
+    % for purposes of figuring out the range, create an additional x, y, z
+    % coordinate
+    x(N+1)=xt(N);
+    y(N+1)=yt(N);
+    z(N+1)=zt(N);
     
+    x_range(sim)=max(x)-min(x);
+    y_range(sim)=max(y)-min(y);
+    z_range(sim)=max(z)-min(z);
+
+    % COMMENT OUT FOR MONTE CARLO SIMS
+%     % Plot the result of 18 linkers 
+%     for i=1:N    
+%         plot3([x(i),x_e(i)],[y(i),y_e(i)],[z(i),z_e(i)],'k');
+%         hold on
+%         plot3([x_e(i),xt(i)],[y_e(i),yt(i)],[z_e(i),zt(i)],'r+:');
+%     end
+    
+end
+toc
+figure()
+hist(x_range)
+figure()
+hist(y_range)
+figure()
+hist(z_range)
+
     
     
     
