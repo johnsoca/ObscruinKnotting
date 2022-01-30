72d4ecda72d636329c5df6a1cbb8266fc6cad625
johnsoca <mill29ca@jmu.edu>
2022-01-30 13:54:18 -0500

    Updated MATLAB code to write data to csv file for R plotting

--- ObscurinKnotting.m
+++ ObscurinKnotting.m
@@ -131,8 +131,16 @@ end
 %hist(x_range);
 %figure()
 %hist(y_range);
-figure()
-hist(crossings);
-figure()
-hist(crossings/N);
+% figure()
+% hist(crossings);
+% figure()
+% hist(crossings/N);
 toc
+
+% Save data into txt file
+M=zeros(sims,4);
+M(:,1)=x_range;
+M(:,2)=y_range;
+M(:,3)=crossings;
+M(:,4)=crossings/N;
+csvwrite('MillionSim2D.txt',M); %NOTE! This will overwrite exisiting file
