90651035e473f2b83d0e1a74770b5b2652fa56cd
johnsoca <cgator@gmail.com>
2022-04-05 21:00:47 -0400

    Finalized debugging and currently working on quantifying crossovers or tangles

--- ObscurinKnotting.m
+++ ObscurinKnotting.m
@@ -31,14 +31,17 @@ for sim = 1:sims
     % Initialize variables
     N = 18;
 
-    X = zeros(2,N+1);
-    E = zeros(2,N);
+    X = zeros(3,N+1);
+    E = zeros(3,N);
+    
+    count=0;
+    thresDist=33/2; % threshold distance for crossing
     
     % Initialize the first position and angle of the first domain of node 1
-    X(:,1) = [rand(); rand()];
+    X(1:2,1) = [rand(); rand()];
     alpha = plus_minus * rand()*pi; % initial orienation of head domain wrt x axis doens't matter 
 
-    E(:,1)=X(:,1)+30*[cos(alpha); sin(alpha)]; %coordinates of the end of the head node
+    E(1:2,1)=X(1:2,1)+30*[cos(alpha); sin(alpha)]; %coordinates of the end of the head node
     
 %----------------------------------------------------
     figure() %comment out if running monte carlo sims
@@ -48,13 +51,13 @@ for sim = 1:sims
 
     % Rotate the coordinate system with respect to alpha using
     % transformation/rotation matrix
-    eP=[cos(alpha), sin(alpha); -sin(alpha), cos(alpha)]*E(:,1);
+    eP=[cos(alpha), sin(alpha); -sin(alpha), cos(alpha)]*E(1:2,1);
         
     phi = plus_minus * normrnd(mu_d2l,s_d2l);
     phi = scaling(phi);
     tP=eP+13*[cos(phi); sin(phi)];
         
-    X(:,2)=[cos(alpha), -sin(alpha); sin(alpha), cos(alpha)]*tP; %coordinates of the end of the tail node, same as start of next head node
+    X(1:2,2)=[cos(alpha), -sin(alpha); sin(alpha), cos(alpha)]*tP; %coordinates of the end of the tail node, same as start of next head node
 % %----------------------------------------------------
 % % CHECK: magnitude of X to E is 30, magnitude of E to X(i+1) is 13, angle 
 % % between EX and EX(i+1)is phi
@@ -74,82 +77,65 @@ for sim = 1:sims
         % Rotate the coordinate system with respect to the prior segment using
         % transformation/rotation matrix
         theta=atan((X(2,i)-E(2,i-1))/(X(1,i)-E(1,i-1)));
-        xP = [cos(theta), sin(theta); -sin(theta), cos(theta)]*X(:,i);
+        xP = [cos(theta), sin(theta); -sin(theta), cos(theta)]*X(1:2,i);
         bimodal = round(rand()+1); % 50% probability of choosing bimodal distribution index 1 as 2
-        alpha=plus_minus*normrnd(mu_l2d(bimodal),s_l2d(bimodal));
+        alpha=scaling(plus_minus*normrnd(mu_l2d(bimodal),s_l2d(bimodal)));
         eP=xP+30*[cos(alpha); sin(alpha)]; %coordinates of the end of the head node
-        E(:,i)=[cos(theta), -sin(theta); sin(theta), cos(theta)]*eP; % coordinates of the point between the domain and linker
+        E(1:2,i)=[cos(theta), -sin(theta); sin(theta), cos(theta)]*eP; % coordinates of the point between the domain and linker
         
         % Rotate the coordinate system with respect to the prior segment
         theta=atan((E(2,i)-X(2,i))/(E(1,i)-X(1,i)));
-        eP= [cos(theta), sin(theta); -sin(theta), cos(theta)]*E(:,i);
-        phi = plus_minus*normrnd(mu_d2l,s_d2l);
+        eP= [cos(theta), sin(theta); -sin(theta), cos(theta)]*E(1:2,i);
+        phi = scaling(plus_minus*normrnd(mu_d2l,s_d2l));
         tP=eP+13*[cos(phi); sin(phi)];
-        X(:,i+1)=[cos(theta), -sin(theta); sin(theta), cos(theta)]*tP; %coordinates of the end of the tail node, same as start of next head node
+        X(1:2,i+1)=[cos(theta), -sin(theta); sin(theta), cos(theta)]*tP; %coordinates of the end of the tail node, same as start of next head node
 %----------------------------------------------------
     plot([X(1,i),E(1,i)],[X(2,i),E(2,i)],'b');
     plot([E(1,i),X(1,i+1)],[E(2,i),X(2,i+1)],'r');
 %----------------------------------------------------
-%----------------------------------------------------
-% CHECK: magnitude of X to E is 30, magnitude of E to X(i+1) is 13, angle 
-% between EX and EX(i+1)is phi, angle between XE(i-1) and XE is alpha
-A=X(:,i)-E(:,i);
-B=X(:,i+1)-E(:,i);
-norm(A) %should be close to 30
-norm(B) % should be close to 13
-phi_test = pi-acos(dot(A,B)/(norm(A)*norm(B))) %should be close to phi
-phi
-
-C=E(:,i-1)-X(:,i);
-D=E(:,i)-X(:,i);
-alpha_test = pi-acos(dot(C,D)/(norm(C)*norm(D))) %should be close to alpha
-alpha
-%----------------------------------------------------
+% %----------------------------------------------------
+% % CHECK: magnitude of X to E is 30, magnitude of E to X(i+1) is 13, angle 
+% % between EX and EX(i+1)is phi, angle between XE(i-1) and XE is alpha
+% A=X(:,i)-E(:,i);
+% B=X(:,i+1)-E(:,i);
+% norm(A) %should be close to 30
+% norm(B) % should be close to 13
+% phi_test = acos(dot(A,B)/(norm(A)*norm(B))) %should be close to phi
+% phi
+% 
+% C=E(:,i-1)-X(:,i);
+% D=E(:,i)-X(:,i);
+% alpha_test = acos(dot(C,D)/(norm(C)*norm(D))) %should be close to alpha
+% alpha
+% %----------------------------------------------------
+        
     end
     x_range(sim) = max(X(1,:))-min(X(1,:));
     y_range(sim) = max(X(2,:))-min(X(2,:));
     
 
-% % % %     % -------------------
-% % % %     % Code below is used to test if a "cross-over" has occurred,
-% % % %     % and count the instances of cross-overs
-% % % %     %--------------------
-% % % %     s = 1;
-% % % %     for i = 1 : N
-% % % %         X(s) = x(i);
-% % % %         X(s+1) = x_e(i);
-% % % %         Y(s) = y(i);
-% % % %         Y(s+1) = y_e(i);
-% % % %         s=s+2;
-% % % %     end
-% % % %     X(37) = xt(18);
-% % % %     Y(37) = yt(18);
-% % % %     
-% % % %     count=0;
-% % % %     for s = 1:length(X)-3
-% % % %         x1 = X(s);
-% % % %         y1 = Y(s);
-% % % %         x2 = X(s+1);
-% % % %         y2 = Y(s+1);
-% % % %         for n = s+2: length(X)-1
-% % % %             x3 = X(n);
-% % % %             y3 = Y(n);
-% % % %             x4 = X(n+1);
-% % % %             y4 = Y(n+1);
-% % % %             c = crossovers(x1,y1,x2,y2,x3,y3,x4,y4);
-% % % %             count = count + c;
-% % % %         end
-% % % %     end
-% % % %     
-% % % %     crossings(sim)=count;
-% % % % %     if crossings(sim)>100
-% % % % %         dX(m,:)=X;
-% % % % %         dY(m,:)=Y;
-% % % % %         dtheta(m,:)=theta_h;
-% % % % %         dphi(m,:)=phi;
-% % % % %         CROSSINGCHECK(m) = crossings(sim);
-% % % % %         m=m+1;
-% % % % %     end
+    % -------------------
+    % Code below is used to test if a "cross-over" has occurred,
+    % and count the instances of cross-overs
+    %--------------------
+    L=zeros(3,N*2);
+    ind=1;
+    for i=1:N
+        L(:,ind)=X(:,i);
+        L(:,ind+1)=E(:,i);
+        ind=ind+2;
+    end
+    
+    for i=1:(N*2)-3
+        for j=(i+2):(N*2)-1
+            [dist vShort]=DistBetween2Segment(L(:,i), L(:,i+1),L(:,j),L(:,j+1));
+            if dist <= thresDist % if linkers and domains are within a specified distance, it "crosses"
+                count=count+1;
+            end
+        end
+    end
+    crossings(sim)=count;
+
 end
 
 %------------------------------------
